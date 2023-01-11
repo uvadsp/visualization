e94e7717d5285beb5b61e9e4998b939f2a9a3c0a
dandevri <hi@dandevri.es>
2023-01-11 17:07:11 +0100

    Add tags page and active link

---
+++ src/routes/tag/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Nav from '../../components/Nav.svelte';
+	import Line from '../../components/Line.svelte';
+</script>
+
+<Nav />
+<main>
+	<Line />
+</main>
+
+<style>
+	main {
+		padding: 2em 2em 2em 7em;
+		width: 90vw;
+		height: 91vh;
+		display: grid;
+		grid-template-columns: repeat(6, 1fr);
+		grid-template-rows: repeat(8, 1fr);
+		grid-row-gap: 2em;
+		grid-column-gap: 2em;
+		font-family: 'Inter', sans-serif;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+		grid-column: 1 / 7;
+		grid-row: 1 / 2;
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+	}
+</style>
