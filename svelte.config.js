f0ce757641c4eae54e039bad64b77b41423e0cb4
dandevri <hi@dandevri.es>
2023-01-13 12:26:20 +0100

    Update svelte versions

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter()
   }
 };
+
+export default config;
